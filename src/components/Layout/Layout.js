6e662cae812d87f0c83607f888901780cc494d2a
Avinash Chavan <avchavan@deloitte.com>
2019-09-25 22:05:47 -0400

    Added side drawer and navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css'
-const Layout = (props) => {
-    return (  
-        <Auxiliary>
-        <div>Sidebar, Manu</div>
-        <main className={classes.Content}>{props.children}</main>
-        </Auxiliary>
-    );
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+class Layout extends Component{
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
+
+    render(){
+        return (  
+            <Auxiliary>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+            <SideDrawer
+                open={this.state.showSideDrawer}
+                closed={this.sideDrawerClosedHandler} />
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+            </Auxiliary>
+        );
+    }
+
 }
  
 export default Layout;
